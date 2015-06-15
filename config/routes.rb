15f4a2922e81f9562b3576e470a6a325a09cb81c
Johan Sveholm <jsveholm@gmail.com>
2015-06-15 15:29:42 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root :to => "pages#show", :id => 1
+end
---
+++ spec/features/display_root_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Display root' do
+
+  it "" do
+    visit root_path
+  end
+
+end
